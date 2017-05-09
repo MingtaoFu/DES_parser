6910767e97f376224daa1bc2b518dcd611f0b4e1
MingtaoFu <madebyrag@gmail.com>
2017-05-09 12:51:02 +0000

    before add branch

---
+++ cmake-build-debug/CMakeFiles/DES_parser.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DES_parser.dir/main.c.o"
+  "DES_parser.pdb"
+  "DES_parser"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/DES_parser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
